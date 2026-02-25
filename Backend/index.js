beabcb3bd07862260d17ba02fe5e9710a531efdb
abhay <abhaypratapsingh7704866570@gmail.com>
2026-02-25 16:22:44 +0530

    Cors Setup dackrize

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
+import dummyData from './api/data.js';
 
 console.log(process.env.PORT);
 const app = express();
@@ -7,8 +9,20 @@ const app = express();
 const PORT = process.env.PORT || 3000; 
 
 
+// CORS setup to allow requests from your frontend
+app.use(cors({
+  origin: process.env.FRONTEND_URL // Dynamically allow your live site
+}));
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello, your server is working!');
+});
+
+
+app.get('/api/data/', (req, res) => {
+  res.json(dummyData);
 });
 
 // Now use that variable in both places
